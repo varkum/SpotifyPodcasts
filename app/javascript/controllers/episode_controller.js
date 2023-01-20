b8b1333106d26343ef3a90624ce102717a3c1caa
Varun Kumar <57076268+varkum@users.noreply.github.com>
2023-01-20 11:16:05 -0500

    play hover

--- app/javascript/controllers/episode_controller.js
+++ app/javascript/controllers/episode_controller.js
@@ -4,6 +4,15 @@ export default class extends Controller {
   static targets = ["episode"];
 
   playHover() {
-    console.log("hovering!");
+    //console.log("hovering!");
+    this.episodeTarget.children[1].setAttribute("style", "opacity: 60%");
+    this.episodeTarget.children[0].classList.remove("hidden");
+    console.log("playhover!");
+  }
+
+  exitHover() {
+    this.episodeTarget.children[1].setAttribute("style", "opacity: 100%");
+    this.episodeTarget.children[0].classList.add("hidden");
+    console.log("exithover!");
   }
 }
