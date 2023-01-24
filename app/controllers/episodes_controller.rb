a11830f48c240c2fbc006090f28526926dbf2508
Varun Kumar <57076268+varkum@users.noreply.github.com>
2023-01-23 20:44:43 -0500

    filters

--- app/controllers/episodes_controller.rb
+++ app/controllers/episodes_controller.rb
@@ -5,14 +5,32 @@ class EpisodesController < ApplicationController
     #GET /episodes
     def index
         @episodes = Current.user.episodes.all
+        session['filters'] = {}
     end
 
     def history
         @episodes = Current.user.episodes.where(status: "done")
+        session['filters'] = {'status': 'done'}
     end
 
     def favorites
         @episodes = Current.user.episodes.where(starred: true)
+        session['filters'] = {'show_starred': 1 }
+    end
+
+
+    #GET /episodes/filter
+    def filter
+        session['filters'] = {} if session['filters'].blank?
+        session['filters'].merge!(filter_params)
+
+        @episodes = Current.user.episodes.all 
+
+        @episodes = @episodes.where("name like ?", "%#{session['filters']['search']}%").or(@episodes.where("show like ?", "%#{session['filters']['search']}%")) if session['filters']['search']
+        @episodes = @episodes.where(status: session['filters']['status']) if session['filters']['status'].present?
+        @episodes = @episodes.where(starred: session['filters']['show_starred']) if session['filters']['show_starred'].present?
+
+        render partial: 'episode_list', locals: {episodes: @episodes}
     end
 
     #GET /episodes/new
@@ -43,10 +61,6 @@ class EpisodesController < ApplicationController
         
     end
 
-    #GET /episodes/filter
-    def filter
-        
-    end
 
     #DELETE /episodes/:id
     def destroy
@@ -64,6 +78,6 @@ class EpisodesController < ApplicationController
     end
 
     def filter_params
-        params.permit(:search, :starred, :starred)
+        params.permit(:search, :status, :show_starred)
     end
 end
